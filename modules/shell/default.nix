ffb57f068388cde83102e05edea286f3782c50d7
MicahPeacock <micah.peacock@proton.me>
2026-01-15 12:46:08 -0700

    Removed limit on one shell active (but still have to choose a default)

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -1,12 +1,6 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.local.programs;
-
-  enabled = lib.count (x: x) [
-    (cfg.bash.enable or false)
-    (cfg.zsh.enable  or false)
-    (cfg.fish.enable or false)
-  ];
 in
 {
   imports = [
@@ -23,22 +17,9 @@ in
     };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf (cfg.shell.default == "bash") {
-      local.programs.bash.enable = lib.mkDefault true;
-    })
-    (lib.mkIf (cfg.shell.default == "zsh") {
-      local.programs.zsh.enable  = lib.mkDefault true;
-    })
-    (lib.mkIf (cfg.shell.default == "fish") {
-      local.programs.fish.enable = lib.mkDefault true;
-    })
-
-    {
-      assertions = [{
-        assertion = enabled <= 1;
-        message = "Only one shell can be enabled.";
-      }];
-    }
-  ];
+  config = {
+    local.programs.bash.enable = lib.mkDefault (cfg.shell.default == "bash");
+    local.programs.zsh.enable  = lib.mkDefault (cfg.shell.default == "zsh");
+    local.programs.fish.enable = lib.mkDefault (cfg.shell.default == "fish");
+  };
 }
